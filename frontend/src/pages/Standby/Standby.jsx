5ae51d20c301104dc1e9a95571f31ffcb5e27693
MAR KEVIN P. ALCANTARA <markevinalcantara40@gmail.com>
2025-10-17 14:17:30 +0800

    changing the time format to hh:mm

--- frontend/src/pages/Standby/Standby.jsx
+++ frontend/src/pages/Standby/Standby.jsx
@@ -178,7 +178,7 @@ export default function Standby() {
     date.toLocaleTimeString('en-US', {
       hour: '2-digit',
       minute: '2-digit',
-      second: '2-digit',
+
       hour12: true,
     });
 
